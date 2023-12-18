c9d207f6465224c2e1b1d255ddaa7cee2faa066a
Leonardo Ferrero-Merlino <leonardo.ferreromerlino@mooney.it>
2023-12-18 17:59:05 +0100

    Added views

---
+++ MailClient/src/main/java/com/mailclient/ReadEmailController.java
@@ -0,0 +1,55 @@
+package com.mailclient;
+
+import com.sharedmodels.Email;
+import com.sharedmodels.ServerRequest;
+import com.sharedmodels.ServerResponse;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.Date;
+import java.util.UUID;
+
+import static com.sharedmodels.MethodType.SEND_EMAIL;
+
+public class ReadEmailController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+
+        try{
+            Socket socket = new Socket("127.0.0.1", 8189);
+            ObjectOutputStream out =
+                    new ObjectOutputStream(socket.getOutputStream());
+
+            Email e = new Email();
+            e.setId(UUID.randomUUID());
+            e.setMailDate(new Date());
+
+            ServerRequest req = new ServerRequest();
+            req.setMethodType(SEND_EMAIL);
+            req.setPayloadType(Email.class);
+            req.setPayload(e);
+
+            out.writeObject(req);
+            System.out.println("Send request");
+
+            ObjectInputStream in =
+                    new ObjectInputStream(socket.getInputStream());
+            ServerResponse nuova = (ServerResponse) in.readObject();
+            System.out.println("Received Response");
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+        catch (ClassNotFoundException ex){
+            ex.printStackTrace();
+        }
+    }
+}
